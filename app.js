1bc87d82f2c07c906fea4b28d28b807f81e4af61
igolubev <igolubevapp@gmail.com>
2020-08-12 15:32:25 +0300

    Add and setup body-parser

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const authRoute = require('./routers/auth.route');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.use('/api/auth', authRoute);
 
--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,9 +1,16 @@
 module.exports.login = (req, res) => {
+  const { email, password } = req.body;
+  
+  console.log(`👨 (${email}) has password: ${password}`);
+
   // TODO: Create login controller logic
   res.status(200).json({ status: 'ok', route: '/api/auth/login', controller: true });
 }
 
 module.exports.registration = (req, res) => {
+  const { email, password } = req.body;
+  console.log(`👨 (${email}) register with password: ${password}`);
+
   // TODO: Create registration controller logic
   res.status(200).json({ status: 'ok', route: '/api/auth/registration', controller: true });
 }
