806be7575a8d8cefae4faae17aacf164413729cc
igolubev <igolubevapp@gmail.com>
2020-08-12 15:56:11 +0300

    Add MongoDB connection ✋

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+const keys = require('./config/keys');
 const authRoute = require('./routers/auth.route');
 
 const app = express();
+
+mongoose.connect(keys.MONGO_URI, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+}).then(() => console.log('✋ Connecting MONGO DB!!!'))
+  .catch(err => console.log('Error:', err));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
